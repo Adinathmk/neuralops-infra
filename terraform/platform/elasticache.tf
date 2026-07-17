a1c7d4fa1b946f47d58b04b625c76dc40ad59e01
Adinathmk <adinathmkclt@gmail.com>
2026-07-17 10:25:39 +0000

    Fix Redis security group: allow ingress from EKS auto-generated cluster SG (worker nodes), not just the unused custom cluster SG

--- terraform/platform/elasticache.tf
+++ terraform/platform/elasticache.tf
@@ -22,6 +22,13 @@ resource "aws_security_group" "redis" {
     security_groups = [aws_security_group.eks_cluster.id]
   }
 
+  ingress {
+    from_port       = 6379
+    to_port         = 6379
+    protocol        = "tcp"
+    security_groups = [aws_eks_cluster.main.vpc_config[0].cluster_security_group_id]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
